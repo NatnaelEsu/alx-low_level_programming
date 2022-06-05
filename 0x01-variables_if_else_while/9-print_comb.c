f20b0c7443b3aeb20475988e1508828edf6f60e5
NatnaelEsu <natnael2021@gmail.com>
2022-06-05 03:08:30 -0700

    comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		if (putchar != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
+}
