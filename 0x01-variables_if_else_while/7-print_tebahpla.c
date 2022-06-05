cd4926093d0d4df9835f15d7709ffb399b6ecd14
NatnaelEsu <natnael2021@gmail.com>
2022-06-05 02:37:43 -0700

    reverse-alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char lower = 'z';
+
+	while (lower >= 'a')
+	{
+		putchar(lower);
+		lower--;
+	}
+	putchar('\n');
+	return (0);
