f7aed7542f0f8316f19296dc677ee2691d88d53c
NatnaelEsu <natnael2021@gmail.com>
2022-06-05 23:27:00 -0700

    100-print

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(True)
+ */
+
+int main(void)
+{
+
+	int i = '0';
+	int j = '0';
+
+	for (i = '0'; i <= '9'; i++)
+	{
+
+		for (j = '0'; j <= '9'; j++)
+		{
+			if (!(i > j || i == J))
+			{
+				putchar(i);
+				putchar(j);
+
+				if (i == '8' && j == '9')
+				{
+					putchar('\n');
+				}
+
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	return (0);
+}
