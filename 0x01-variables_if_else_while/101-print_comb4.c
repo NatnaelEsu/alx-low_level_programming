be9ba8268a3b60dc7fc6119c4164316db379585a
NatnaelEsu <natnael2021@gmail.com>
2022-06-06 00:26:01 -0700

    101-print

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Ture)
+ *
+ */
+
+int main(void)
+{
+	int i = '0';
+	int j = '0';
+	int k = '0';
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				if (i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+
+					if (!(i == '7' && j == '8' && k == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+
+					else
+					{
+						putchar('\n');
+					}
+				}
+			}
+		}
+	}
+
+	return (0);
+}
