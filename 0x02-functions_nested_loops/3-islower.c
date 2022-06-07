6a8dcf8ad737ba19715886f3ea4ce15b641f18e7
NatnaelEsu <natnael2021@gmail.com>
2022-06-07 11:38:09 -0700

    lower case letter

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,18 +2,6 @@
 #include "main.h"
 
 /**
- * main - check code
- *
- * Return: Always 0(True)
- */
-
-int main(void)
-{
-	int _islower(int c);
-	return (0);
-}
-
-/**
  * _islower - to check a character for lowercase
  * @c: the variable that needs to be checked
  *
