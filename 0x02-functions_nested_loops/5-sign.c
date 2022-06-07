c12b56e1dcf962dd81d91d6164512273047c26a3
NatnaelEsu <natnael2021@gmail.com>
2022-06-07 11:42:18 -0700

    number sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,18 +2,6 @@
 #include "main.h"
 
 /**
- * main -Entry point
- *
- * Return: Always 0 True
- */
-
-int main(void)
-{
-	int print_sign(int n);
-	return (0);
-}
-
-/**
  * print_sign - check if it is a letter
  * @n: the input that is going to be checked
  *
