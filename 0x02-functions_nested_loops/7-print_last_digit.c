2f20ea680cc3f116e129878f00b8dc2a5540f0c9
NatnaelEsu <natnael2021@gmail.com>
2022-06-07 12:13:45 -0700

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - absolute value of an integer
+ * @r: the input that is going to be checked
+ *
+ * Return: return the absolute value of r
+ */
+
+int print_last_digit(int r)
+{
+	r = r % 10;
+	return (r);
+}
