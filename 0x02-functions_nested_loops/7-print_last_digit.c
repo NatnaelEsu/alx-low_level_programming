a1bb22e8a438f67c92dcdc989d7aac17174c3887
NatnaelEsu <natnael2021@gmail.com>
2022-06-07 12:47:21 -0700

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,6 +10,18 @@
 
 int print_last_digit(int r)
 {
-	r = r % 10;
+	if (r < 0)
+	{
+		r = r * -1;
+		r = r % 10;
+		_putchar(r + '0');
+	}
+
+	else
+	{
+		r = r % 10;
+		_putchar(r + '0');
+	}
+
 	return (r);
 }
