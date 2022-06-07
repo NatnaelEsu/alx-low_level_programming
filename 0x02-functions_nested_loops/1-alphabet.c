de73e5d3ad8174f68d97c3689259e42137d2cd78
NatnaelEsu <natnael2021@gmail.com>
2022-06-07 00:17:06 -0700

    print _alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,31 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(True)
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0(True)
+ */
+
+void print_alphabet(void)
+{
+	int i = 'a';
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
