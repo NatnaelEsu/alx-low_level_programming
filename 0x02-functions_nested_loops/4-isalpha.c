2ff1f0bcb97c795c6c8fd6a7d85fa56e3b4f9758
NatnaelEsu <natnael2021@gmail.com>
2022-06-07 11:40:19 -0700

    uppercae letters

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,18 +2,6 @@
 #include "main.h"
 
 /**
- * main -Entry point
- *
- * Return: Always 0 True
- */
-
-int main(void)
-{
-	int _isalpha(int c);
-	return (0);
-}
-
-/**
  * _isalpha - check if it is a letter
  * @c: the input that is going to be checked
  *
