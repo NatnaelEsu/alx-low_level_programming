6cb626808daa38f9e1009ad751e58349e8db5342
NatnaelEsu <natnael2021@gmail.com>
2022-06-10 00:23:21 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - function performs multiplication
+ * @a: first digit
+ * @b: second digit
+ *
+ * Return: multiplication
+ */
+
+int mul(int a, int b)
+{
+	int r;
+
+	r = a * b;
+	return (r);
+}
