721dd82fd459ed904e44bf617b161ff08e9f7c15
NatnaelEsu <natnael2021@gmail.com>
2022-06-10 03:34:51 -0700

    this is triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle with # pattern
+ * @size: number of row and column
+ *
+ * Return: nothing
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = size; j >= 1; j--)
+			{
+				if (i < j)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
