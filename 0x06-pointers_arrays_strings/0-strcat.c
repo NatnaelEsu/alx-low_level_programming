6c154a22bc2015c3c4c408388ce054b846965f8d
NatnaelEsu <natnael2021@gmail.com>
2022-06-15 22:51:02 -0700

    concatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: input parameter string
+ * @src: input parameter string
+ *
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	a = 0;
+	b = 0;
+
+	while (dest[i] != '\0')
+	{
+		a++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		a++;
+		b++;
+	}
+
+	return (dest);
+}
