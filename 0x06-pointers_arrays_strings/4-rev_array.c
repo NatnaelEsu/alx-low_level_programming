2cf321ad4f9d7267efe091263daabe9869e0f851
NatnaelEsu <95136911+NatnaelEsu@users.noreply.github.com>
2022-06-16 12:39:30 +0300

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+ * reverse_array - function to reverse array                                                   
+ * @a: input array                                                                             
+ * @n: number of element                                                                       
+ * Return: reverse array                                                                       
+ */                                                                                            
+                                                                                               
+void reverse_array(int *a, int n)                                                              
+{                                                                                              
+        int i;                                                                                 
+        int temp;                                                                              
+                                                                                               
+        i = 0;                                                                                 
+        n = n - 1;                                                                             
+                                                                                               
+        while (i < n)                                                                          
+        {                                                                                      
+                temp = a[i];                                                                   
+                a[i] = a[n];                                                                   
+                a[n] = temp;                                                                   
+                i++;                                                                           
+                n--;                                                                           
+        }                                                                                      
+}
